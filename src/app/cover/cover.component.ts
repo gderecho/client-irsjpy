d16492f8efd680dd703d15ca7a60f2e3c2bb3717
gderecho <gderecho@users.noreply.github.com>
2020-04-18 19:49:03 +0900

    Set up bootstrap, cover component, basic skeleton
    cover component based on https://getbootstrap.com/docs/4.0/examples/cover/

---
+++ src/app/cover/cover.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cover',
+  templateUrl: './cover.component.html',
+  styleUrls: ['./cover.component.scss']
+})
+export class CoverComponent implements OnInit {
+
+  data = {
+    a: "a",
+    b: "b",
+    c: "c"
+  };
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
