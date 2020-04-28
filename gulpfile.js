602402328f7e693f1067dff73306a026fb87edae
gderecho <gderecho@users.noreply.github.com>
2020-04-29 01:44:40 +0900

    Fix replace script by renaming templates instead

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
 const gulp = require('gulp');
 const exec = require('child_process').exec
 
-gulp.task('replace', (callback) => {
-    exec('node ./replace.js', (err, stdout, stderr) => {
+gulp.task('generate-env', (callback) => {
+    exec('bash ./generate_env.sh', (err, stdout, stderr) => {
         console.log(stdout);
         console.log(stderr);
         callback()
